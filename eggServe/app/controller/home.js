a06de639bb70d3d4120f14c46c945f9c7e402d83
hudandan <hudandan@cygmgs.com>
2020-09-18 18:05:21 +0800

    连接数据库

--- eggServe/app/controller/home.js
+++ eggServe/app/controller/home.js
@@ -4,7 +4,9 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const { ctx } = this;
+    const { ctx, app } = this;
+    const res = await app.mysql.select('article');
+    console.log(res)
     ctx.body = 'hi, egg';
   }
 }
